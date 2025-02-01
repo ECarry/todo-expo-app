72077d848fae34388ecc75179ea3651e29f32e2c
ecarry <lianshiliang93@gmail.com>
2025-02-01 21:45:32 +0800

    init project with NativeWind

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Home() {
+  return (
+    <SafeAreaView>
+      <Text className="text-3xl font-bold text-blue-500">Home</Text>
+    </SafeAreaView>
+  );
+}
